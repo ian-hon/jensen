cd6cda16278efd89cf9bb50ce9ddae62f6a0dc5d
ajian_nedo <asianerd28@gmail.com>
2025-11-06 18:21:31 +0800

    jensen

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <div className={styles.parent}>
+            <Image className={styles.background} src='/waves.jpg' alt='' height='500' width='500' />
+            <div className={styles.container}>
+                <Image className={styles.jensen} src="/jensen.png" alt="" height="500" width="500" />
+                <div className={styles.description}>
+                    <h1>hi im jensen</h1>
+                    <h2>i like suhing dih</h2>
+                    <h2>i luh dih in ma buh 😛</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
